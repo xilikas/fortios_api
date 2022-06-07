82889d645a998f86545d50c317de1f9538daf4e0
Fil Luzadas <flg.l@outlook.com>
2022-06-07 01:46:05 -0400

    Gets single policy using API call than iterating

--- policies.py
+++ policies.py
@@ -3,16 +3,14 @@ View and edit firewall policies, as well as including an interface to use firewa
 """
 
 import requests 
+# Disable InsecureRequestWarning
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 key = os.getenv('API_KEY')
-url = f'http://firewall.xilikas/api/v2/cmdb/firewall/policy?access_token={key}' 
-
-response = requests.get(url, verify=False)
-policies = response.json()['results']
 
 def view_policies(arg_id = 0):
     """
@@ -23,16 +21,14 @@ def view_policies(arg_id = 0):
 
     Returns: 
     prints commonly viewed attributes of a policy
-
-    Todo: 
-    Add optional parameter, iterate through all policies
     """
 
-    counter = 0
-
-    for policy in policies: 
-        if arg_id == 0: 
-            # if arg_id == 0, iterate through all policies and list them 
+    if arg_id == 0:
+        url = f'http://firewall.xilikas/api/v2/cmdb/firewall/policy?access_token={key}'
+        response = requests.get(url, verify=False)
+        policies = response.json()['results']
+        counter = 0
+        for policy in policies:
             id = policies[counter]['policyid']
             name = policies[counter]['name']
             srcintf = policies[counter]['srcintf'][0]['name']
@@ -50,30 +46,33 @@ def view_policies(arg_id = 0):
             print(f'Action: {action}')
 
             counter += 1
-        else: 
-            # if arg_id != 0, iterate through all policies until policy id == arg_id, then display that policy only
-            if arg_id == policies[counter]['policyid']:
-                id = policies[counter]['policyid']
-                name = policies[counter]['name']
-                srcintf = policies[counter]['srcintf'][0]['name']
-                dstintf = policies[counter]['dstintf'][0]['name']
-                srcaddr = policies[counter]['srcaddr'][0]['name']
-                dstaddr = policies[counter]['dstaddr'][0]['name']
-                service = policies[counter]['service'][0]['name']
-                action = policies[counter]['action']
+    else:
+        url = f'http://firewall.xilikas/api/v2/cmdb/firewall/policy/{arg_id}?access_token={key}'
+        response = requests.get(url, verify=False)
+        if response.json()['http_status'] == 200:
+            policy = response.json()['results']
+            id = policy[0]['policyid']
+            name = policy[0]['name']
+            srcintf = policy[0]['srcintf'][0]['name']
+            dstintf = policy[0]['dstintf'][0]['name']
+            srcaddr = policy[0]['srcaddr'][0]['name']
+            dstaddr = policy[0]['dstaddr'][0]['name']
+            service = policy[0]['service'][0]['name']
+            action = policy[0]['action']
 
-                print(f'Policy ID: {id}')
-                print(f'Name: {name}')
-                print(f'Interface: {srcintf} -> {dstintf}')
-                print(f'Address: {srcaddr} -> {dstaddr}')
-                print(f'Service: {service}')
-                print(f'Action: {action}')
-
-                break
+            print(f'Policy ID: {id}')
+            print(f'Name: {name}')
+            print(f'Interface: {srcintf} -> {dstintf}')
+            print(f'Address: {srcaddr} -> {dstaddr}')
+            print(f'Service: {service}')
+            print(f'Action: {action}')
             
-            counter += 1
+        else:
+            print('Policy ID does not exist.')
 
 print ('All policies:')
 view_policies()
 print ('Only one policy (e.g. 3):')
 view_policies(3)
+print ('Error message for when policy ID does not exist:')
+view_policies(5)
